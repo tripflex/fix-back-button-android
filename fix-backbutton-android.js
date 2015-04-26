f6c12926d0819eafb3910ffc264ab602023f0020
Remco Veldkamp <remcoder@gmail.com>
2015-04-26 20:19:38 +0200

    fixed typo in package name

--- fix-backbutton-android.js
+++ fix-backbutton-android.js
@@ -1,8 +1,8 @@
-// requires Suspend plugin (org.android.tools.suspend)
-// see: https://github.com/Lamerchun/org.android.tools.suspend
-Meteor.startup(function() {
-  if (Meteor.isCordova)
-    document.addEventListener("backbutton", function () {
-      window.plugins.Suspend.suspendApp();
+    // requires Suspend plugin (org.android.tools.suspend)
+    // see: https://github.com/Lamerchun/org.android.tools.suspend
+    Meteor.startup(function() {
+      if (Meteor.isCordova)
+        document.addEventListener("backbutton", function () {
+          window.plugins.Suspend.suspendApp();
+        });
     });
-});
--- package.js
+++ package.js
@@ -1,22 +1,17 @@
 Package.describe({
-  name: 'remcoder:fix-backbutton-android',
+  name: 'remcoder:fix-back-button-android',
   version: '0.0.1',
   summary: 'Fixes the native backbutton on Android',
-  git: 'https://github.com/remcoder/fix-backbutton-android',
+  git: 'https://github.com/remcoder/fix-back-button-android',
   documentation: 'README.md'
 });
 
 Package.onUse(function(api) {
   api.versionsFrom('1.1.0.2');
-  api.addFiles('fix-backbutton-android.js');
+  api.addFiles('fix-back-button-android.js');
 });
 
 Cordova.depends({
   'org.android.tools.suspend':'0.1.2'
 });
 
-Package.onTest(function(api) {
-  api.use('tinytest');
-  api.use('remcoder:fix-backbutton-android');
-  api.addFiles('fix-backbutton-android-tests.js');
-});
