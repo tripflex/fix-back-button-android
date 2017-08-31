e929b7ea64aa5a7e2cbeedf47619d6c611178b62
Myles McNamara <tripflex@users.noreply.github.com>
2017-08-31 12:06:50 -0400

    use github repo for org.android.tools.suspend
    org.android.tools.suspend no longer works when meteor attempts to install via npm, update cordova depends to use github

--- package.js
+++ package.js
@@ -1,8 +1,8 @@
 Package.describe({
-  name: 'remcoder:fix-back-button-android',
-  version: '0.0.1',
-  summary: 'Fixes the native back button on Android',
-  git: 'https://github.com/remcoder/fix-back-button-android',
+  name: 'tripflex:fix-back-button-android',
+  version: '0.0.2',
+  summary: 'Fixes the native back button on Android (fork of remcoder updated to work with org.android.tools.suspend)',
+  git: 'https://github.com/tripflex/fix-back-button-android',
   documentation: 'README.md'
 });
 
@@ -12,6 +12,6 @@ Package.onUse(function(api) {
 });
 
 Cordova.depends({
-  'org.android.tools.suspend':'0.1.2'
+  'org.android.tools.suspend':'https://github.com/Lamerchun/org.android.tools.suspend.git#0dbb52cca0244ba22a8c7975895f0f45d2e9a4a9'
 });
 
