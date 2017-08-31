91d140da9b55fee3d4edd0cf1a9bd03573153ab1
Myles McNamara <tripflex@gmail.com>
2017-08-31 12:36:36 -0400

    update meteor summary to less than 100 chars

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
   name: 'tripflex:fix-back-button-android',
   version: '0.0.2',
-  summary: 'Fixes the native back button on Android (fork of remcoder updated to work with org.android.tools.suspend)',
+  summary: 'Fixes the native back button on Android (fork of remcoder to fix org.android.tools.suspend)',
   git: 'https://github.com/tripflex/fix-back-button-android',
   documentation: 'README.md'
 });
@@ -14,4 +14,3 @@ Package.onUse(function(api) {
 Cordova.depends({
   'org.android.tools.suspend':'https://github.com/Lamerchun/org.android.tools.suspend.git#0dbb52cca0244ba22a8c7975895f0f45d2e9a4a9'
 });
-
